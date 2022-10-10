eaf33183b9a558379bd6d110c61932c5237a524c
juhwlong <jwlong6@ilstu.edu>
2022-10-10 16:22:09 -0500

    new

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -1,12 +1,18 @@
 let state = -1;
 let s1,s2,s3;
+let i1,i2,i3;
 function preload(){
   s1= loadSound("assets/allthat.mp3");
   s2= loadSound("assets/creativeminds.mp3");
   s3= loadSound("assets/newdawn.mp3");
+  
 }
 function setup() {
   createCanvas(800, 800);
+  i1 = loadImage("assets/Bee.jpg");
+  i2 = loadImage("assets/Cat.jpg");
+  i3 = loadImage("assets/Otter.jpg");
+  imageMode(CENTER);
 }
 
 function draw() {
@@ -22,6 +28,7 @@ function draw() {
       if(!s1.isPlaying()){
         s1.play();
       }
+      image(i1,width/2,height/2, 150,150);
       text("All That", 100, 100);
       break;
 
@@ -30,6 +37,7 @@ function draw() {
         s2.play();
       }
       background("green");
+      image(i2,width/2,height/2, 150,150);
       text("Creative Minds", 100, 100);
       break;
 
@@ -38,6 +46,7 @@ function draw() {
         s3.play();
       }
       background("orange");
+      image(i3,width/2,height/2, 150,150);
       text("New Dawn", 100, 100);
       break;
 
